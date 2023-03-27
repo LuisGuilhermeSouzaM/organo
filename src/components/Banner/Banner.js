3cd3f971d9f01cd5ce888c7be0cf2b1ceb21b48e
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2023-03-27 20:52:05 -0300

    lesson 1 course react with js (Banner, TextField, Form)

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+
+export const Banner = () => {
+    return (
+        <header className="banner">
+            <img src="/assets/images/banner.png" alt="o banner principal da página do Organo" />
+        </header>
+    );
+};
---
+++ src/components/TextField/TextField.js
@@ -0,0 +1,16 @@
+import './TextField.css'
+
+
+
+export const TextField = (props) => 
+{
+    const modifiedPlaceholder =  `${props.placeholder}...`
+    return (
+        <div className="text-field">
+            <label>
+                {props.label}
+            </label>
+            <input placeholder={modifiedPlaceholder}/>
+        </div>
+    )
+}
