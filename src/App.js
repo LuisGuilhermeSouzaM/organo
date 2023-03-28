f1699e8dc0c46c9e719ad3231704bd68ac67c158
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2023-03-28 10:36:29 -0300

    aula 2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import { useState } from 'react';
-import Banner from './componentes/Banner';
-import Formulario from './componentes/Formulario';
-import Categoria from './componentes/Categoria';
-import categorias from './constants';
+import { useState } from "react";
+import Banner from "./componentes/Banner";
+import Formulario from "./componentes/Formulario";
+import Rodape from "./componentes/Rodape";
+import Categoria from "./componentes/Time";
+import { categorias, inicial } from './constants';
 
 function App() {
 
-  const [colaboradores, setColaboradores] = useState([])
 
-  const aoNovoColaboradorAdicionado = (colaborador) => {
-    debugger
-    setColaboradores([...colaboradores, colaborador])
-  }
+  const [colaboradores, setColaboradores] = useState(inicial)
+
 
   return (
-    <div className="App">
+    <div>
       <Banner />
-      <Formulario categorias={categorias.map(categoria => categoria.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      
+      <Formulario categorias={categorias.map(categoria => categoria.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
       <section className="categorias">
-        {categorias.map(categoria => <Categoria 
-          key={categoria.nome} 
-          nome={categoria.nome} 
-          corPrimaria={categoria.corPrimaria} 
-          corSecundaria={categoria.corSecundaria} 
-          colaboradores={colaboradores.filter(colaborador => colaborador.categoria === categoria.nome)}
-        />)}   
+        <h1>Minha organização</h1>
+        {categorias.map((categoria, indice) => <Categoria key={indice} categoria={categoria} colaboradores={colaboradores.filter(colaborador => colaborador.categoria === categoria.nome)} />)}
       </section>
       <Rodape />
     </div>
